4340dba335fe6f2761c77528f64175ae8994e2f9
sweezy793 <sarthaks793@gmail.com>
2018-08-24 00:05:13 +0530

    Schema creation

--- customer.js
+++ customer.js
@@ -1,5 +1,10 @@
 const mongoose=require('mongoose');
 
 const studentSchema=mongoose.Schema({
-    
-})
+    firstname:{type:String},
+    lastname:{type:String},
+    subject:{type:String},
+    present:{type:Boolean}
+});
+
+module.exports=mongoose.model('Student',studentSchema);
