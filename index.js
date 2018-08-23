1eed1aabf57f8266e9e8fe335b513156361e41c8
sweezy793 <sarthaks793@gmail.com>
2018-08-24 00:48:01 +0530

    Student data can be searched but just with name

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const mongoose=require('mongoose');
-
+const Student=require('./models/student');
 //to get rid of warning
 mongoose.Promise=global.Promise;
 
 //connects to db
 const db=mongoose.connect('mongodb://localhost:27017/attendancecli',{useMongoClient:true});
+
+//add student
+const addStudent=(student)=>{
+    Student.create(student).then(student=>{
+        console.info('Student data added');
+        db.close();
+    });
+}
+
+//find student by name
+const findStudent=(name)=>{
+
+    const search=new RegExp(name,'i');
+    Student.find({$or:[{firstname:search},{lastname:search}]})
+    .then(student=>{
+        console.info(student);
+        console.info(`${student.length} matches`);
+        db.close();
+    });
+}
+
+module.exports={
+    addStudent,
+    findStudent
+}
+ 
