2a3e952cdb7730f11cc40d6b1874f279d9693d06
sweezy793 <sarthaks793@gmail.com>
2018-08-24 22:09:54 +0530

    Asks questions now

--- commander.js
+++ commander.js
@@ -1,21 +1,60 @@
 const program=require('commander');
+const {prompt}=require('inquirer');
 const {
     addStudent,
     findStudent,
     findPresent
 }=require('./index');
 
+
+//Questions
+const questions=[
+    {
+        type:'input',
+        name:'firstname',
+        message:`Student's first name`
+    },
+    {
+        type:'input',
+        name:'lastname',
+        message:`Student's last name`
+    },
+    {
+        type:'input',
+        name:'rollno',
+        message:`Student's Roll No.`
+    },
+    {
+        type:'input',
+        name:'subject',
+        message:`Subject name`
+    },
+    {
+        type:'input',
+        name:'present',
+        message:`Present?`
+    }
+]
+
 program
 .version('1.0.0')
 .description('Student Management System')
 
+// program
+// .command('add <firstname> <lastname> <rollno> <subject> <present>')
+// .alias('a')
+// .description('Add a student')
+// .action((firstname,lastname,rollno,subject,present)=>{
+//     addStudent({firstname,lastname,rollno,subject,present});    
+// });
+
 program
-.command('add <firstname> <lastname> <subject> <present>')
+.command('add')
 .alias('a')
 .description('Add a student')
-.action((firstname,lastname,subject,present)=>{
-    addStudent({firstname,lastname,subject,present});    
-});
+.action(()=>{
+    prompt(questions).then(answers=>addStudent(answers));
+}); 
 
 program
 .command('find <name>')
